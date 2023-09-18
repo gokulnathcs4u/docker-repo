d12d7b2f8348fc6efb4670c9067875aee1c38744
gokulnathcs4u <gokulnathcs4u@gmail.com>
2023-09-18 11:57:01 +0530

    added deploy.yml file

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from "./App";
 
 test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/bi there/i);
+  const linkElement = screen.getByText("bi there");
   expect(linkElement).toBeInTheDocument();
 });
